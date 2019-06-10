743e6a70b151e13e739dc72bc9694484147b6a9b
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-06-09 22:11:49 -0300

    Final-project

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var map;
 function initMap() {
     map = new google.maps.Map(
         document.getElementById('map'),
-        { center: new google.maps.LatLng(-12.255423, -38.941116), zoom: 16 });
+        { center: new google.maps.LatLng(-12.255423, -38.941116), zoom: 17 });
 
     var iconBase =
         'assets/imgs/';
